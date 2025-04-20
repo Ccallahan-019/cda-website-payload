082e12305cc12d676894575926aefab87bf40bb3
Christian Callahan <callahan.0019@gmail.com>
2025-04-20 01:06:26 -0400

    write out home page

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,4 +1,42 @@
-import PageTemplate
- from "./[...slug]/page";
+import { notFound } from "next/navigation";
+import { getApolloClient } from "@/graghql/apolloClient";
+import { GET_PAGE_BY_SLUG } from "@/graghql/queries/pageQuery";
+import { RenderBlocks } from "@/blocks/RenderBlocks";
+import { RenderHero } from "@/heros/RenderHero";
+import type { Document } from "payload";
+import { cookies, draftMode } from "next/headers";
+import { LivePreviewListener } from "@/components/live-preview-listener/LivePreviewListener";
 
-export default PageTemplate;
+export default async function HomePage() {
+  const { isEnabled: draft } = await draftMode();
+  const slug = "home";
+
+  const cookieStore = await cookies();
+  const token = draft ? cookieStore.get("payload-token")?.value : undefined;
+
+  const client = getApolloClient(token);
+
+  try {
+    const { data } = await client.query({
+      query: GET_PAGE_BY_SLUG,
+      variables: { slug: slug, draft },
+    });
+
+    const page: Document | null = data.Pages?.docs?.[0] || null;
+
+    if (!page) return notFound();
+
+    const { hero, layout } = page;
+
+    return (
+      <article>
+        {draft && <LivePreviewListener />}
+        <RenderHero {...hero} />
+        <RenderBlocks blocks={layout} />
+      </article>
+    );
+  } catch (err) {
+    console.error("Error fetching page data:", err);
+    return notFound();
+  }
+}
