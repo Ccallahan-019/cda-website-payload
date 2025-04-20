c6d5d16868e967419760ec148f603a0a7b7d8576
Christian Callahan <callahan.0019@gmail.com>
2025-04-19 23:11:25 -0400

    update fetch and ssrMode

--- graghql/apolloClient.ts
+++ graghql/apolloClient.ts
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import fetch from 'cross-fetch';
 
 export function getClient(token?: string) {
   return new ApolloClient({
-    ssrMode: true,
+    ssrMode: typeof window === 'undefined',
     link: new HttpLink({
       uri: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/graphql`, // your Payload GraphQL URL
       fetch,
