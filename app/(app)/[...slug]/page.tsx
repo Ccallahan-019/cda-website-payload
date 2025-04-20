7ffc178558d40a80f9a9b5147f5d6b960b35116c
Christian Callahan <callahan.0019@gmail.com>
2025-04-20 00:41:38 -0400

    move from SSG to SSR

--- app/(app)/[...slug]/page.tsx
+++ app/(app)/[...slug]/page.tsx
@@ -1,75 +1,47 @@
 import { notFound } from "next/navigation";
 import { getApolloClient } from "@/graghql/apolloClient";
 import { GET_PAGE_BY_SLUG } from "@/graghql/queries/pageQuery";
-import { GET_SLUGS } from "@/graghql/queries/slugQuery";
 import { RenderBlocks } from "@/blocks/RenderBlocks";
 import { RenderHero } from "@/heros/RenderHero";
 import type { Document } from "payload";
 import { cookies, draftMode } from "next/headers";
 import { LivePreviewListener } from "@/components/live-preview-listener/LivePreviewListener";
 
-export async function generateStaticParams() {
-  const client = getApolloClient();
-  const pages = await client.query({
-    query: GET_SLUGS,
-  });
-
-  const params = pages.data.Pages.docs
-    ?.filter((doc: Document) => {
-      return doc.slug !== 'home'
-    })
-    .map((doc: Document) => ({
-      slug: doc.slug.split('/')
-    }));
-
-    return params;
-}
-
-const queryPageBySlug = async ({ path }: { path: string }) => {
+export default async function PageTemplate({
+  params,
+}: {
+  params: { slug?: string[] };
+}) {
   const { isEnabled: draft } = await draftMode();
+  const slug = params.slug || ["home"];
+  const path = slug.join("/");
+
   const cookieStore = await cookies();
-  const token = draft ? cookieStore.get('payload-token')?.value : undefined;
+  const token = draft ? cookieStore.get("payload-token")?.value : undefined;
 
   const client = getApolloClient(token);
-  
-  const { data } = await client.query({
-    query: GET_PAGE_BY_SLUG,
-    variables: { slug: path, draft },
-  });
 
-  return data.Pages.docs[0] || null;
-}
+  try {
+    const { data } = await client.query({
+      query: GET_PAGE_BY_SLUG,
+      variables: { slug: path, draft },
+    });
 
-type Args = {
-  params: Promise<{
-    slug?: string[]
-  }>
-}
-
-export default async function PageTemplate({ params: paramsPromise }: Args) {
-    const { isEnabled: draft } = await draftMode();
-    const { slug = ['home'] } = await paramsPromise;
-    const path = slug.join('/')
-
-    const page = await queryPageBySlug({
-      path,
-    })
-
-    if (!page) {
-      return notFound();
-    }
-
-    const { hero, layout } = page;
+    const page: Document | null = data.Pages?.docs?.[0] || null;
 
     if (!page) return notFound();
 
-  return (
-    <article>
-      {draft && <LivePreviewListener />}
-      
+    const { hero, layout } = page;
 
-      <RenderHero {...hero} />
-      <RenderBlocks blocks={layout} />
-    </article>
-  );
+    return (
+      <article>
+        {draft && <LivePreviewListener />}
+        <RenderHero {...hero} />
+        <RenderBlocks blocks={layout} />
+      </article>
+    );
+  } catch (err) {
+    console.error("Error fetching page data:", err);
+    return notFound();
+  }
 }
