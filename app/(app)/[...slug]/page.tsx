aa132462b2e4eed378bf7babcf44304ef8e3e857
Christian Callahan <callahan.0019@gmail.com>
2025-04-20 21:23:59 -0400

    move to ISR

--- app/(app)/[...slug]/page.tsx
+++ app/(app)/[...slug]/page.tsx
@@ -1,11 +1,47 @@
 import { notFound } from "next/navigation";
 import { getApolloClient } from "@/graghql/apolloClient";
 import { GET_PAGE_BY_SLUG } from "@/graghql/queries/pageQuery";
+import { GET_SLUGS } from "@/graghql/queries/slugQuery";
 import { RenderBlocks } from "@/blocks/RenderBlocks";
 import { RenderHero } from "@/heros/RenderHero";
 import type { Document } from "payload";
 import { cookies, draftMode } from "next/headers";
 import { LivePreviewListener } from "@/components/live-preview-listener/LivePreviewListener";
+
+export const revalidate = 60;
+export const dynamicParams = true;
+
+export async function generateStaticParams() {
+  const client = getApolloClient();
+  const pages = await client.query({
+    query: GET_SLUGS,
+  });
+
+  const params = pages.data.Pages.docs
+    ?.filter((doc: Document) => {
+      return doc.slug !== 'home'
+    })
+    .map((doc: Document) => ({
+      slug: doc.slug.split('/')
+    }));
+
+    return params;
+}
+
+const queryPageBySlug = async ({ path }: { path: string }) => {
+  const { isEnabled: draft } = await draftMode();
+  const cookieStore = await cookies();
+  const token = draft ? cookieStore.get('payload-token')?.value : undefined;
+
+  const client = getApolloClient(token);
+  
+  const { data } = await client.query({
+    query: GET_PAGE_BY_SLUG,
+    variables: { slug: path, draft },
+  });
+
+  return data.Pages.docs[0] || null;
+}
 
 type Args = {
   params: Promise<{
@@ -13,37 +49,30 @@ type Args = {
   }>
 }
 
-export default async function PageTemplate({ params }: Args) {
-  const { isEnabled: draft } = await draftMode();
-  const slug = (await params).slug || ["home"];
-  const path = slug.join("/");
+export default async function PageTemplate({ params: paramsPromise }: Args) {
+    const { isEnabled: draft } = await draftMode();
+    const { slug = ['home'] } = await paramsPromise;
+    const path = slug.join('/')
 
-  const cookieStore = await cookies();
-  const token = draft ? cookieStore.get("payload-token")?.value : undefined;
+    const page = await queryPageBySlug({
+      path,
+    })
 
-  const client = getApolloClient(token);
-
-  try {
-    const { data } = await client.query({
-      query: GET_PAGE_BY_SLUG,
-      variables: { slug: path, draft },
-    });
-
-    const page: Document | null = data.Pages?.docs?.[0] || null;
-
-    if (!page) return notFound();
+    if (!page) {
+      return notFound();
+    }
 
     const { hero, layout } = page;
 
-    return (
-      <article>
-        {draft && <LivePreviewListener />}
-        <RenderHero {...hero} />
-        <RenderBlocks blocks={layout} />
-      </article>
-    );
-  } catch (err) {
-    console.error("Error fetching page data:", err);
-    return notFound();
-  }
+    if (!page) return notFound();
+
+  return (
+    <article>
+      {draft && <LivePreviewListener />}
+      
+
+      <RenderHero {...hero} />
+      <RenderBlocks blocks={layout} />
+    </article>
+  );
 }
