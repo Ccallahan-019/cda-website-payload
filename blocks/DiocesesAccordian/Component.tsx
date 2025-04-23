735150831269b4b6cf8b17a817691fea0b102be6
Christian Callahan <callahan.0019@gmail.com>
2025-04-22 21:16:16 -0400

    add richText to component, add responsive styles, add border

--- blocks/DiocesesAccordian/Component.tsx
+++ blocks/DiocesesAccordian/Component.tsx
@@ -1,13 +1,15 @@
 import { DiocesesAccordianBlock as DiocesesAccordianBlockProps } from "@/payload-types";
 import AccordianCard from "@/components/accordian/AccordianCard";
+import RichText from "@/lexical-components/RichText";
 
 export const DiocesesAccordianBlock: React.FC<DiocesesAccordianBlockProps> = (props) => {
-    const { dioceses } = props
+    const { dioceses, richText } = props
 
     return (
-        <div className="container my-16 sm:my-20">
+        <div className="container my-16 sm:my-20 space-y-4">
+            {richText && <RichText data={richText} />}
             <div className="rounded-xl bg-background/60 backdrop-blur-sm shadow-xl">
-                <div className="divide-y divide-gray-300">
+                <div className="divide-y divide-gray-400">
                     {Array.isArray(dioceses) && dioceses.length > 0 && (
                         dioceses.map((diocese, index) => {
                             if (diocese && typeof diocese === 'object') {
--- components/accordian/AccordianCard.tsx
+++ components/accordian/AccordianCard.tsx
@@ -26,7 +26,7 @@ export default function AccordianCard({ diocese }: Props) {
         <div>
             <button
                 onClick={toggleExpanded}
-                className="py-8 px-4 sm:px-6 flex items-center justify-between w-full text-lg font-semibold text-left text-gray-900"
+                className="py-8 px-4 sm:px-6 flex items-center justify-between w-full text-xl sm:text-2xl font-semibold text-left text-gray-900"
             >
                 <span>{dioceseName}</span>
                 <span className="ml-4">
@@ -39,8 +39,8 @@ export default function AccordianCard({ diocese }: Props) {
             </button>
 
             <AnimateDropdown isExpanded={isExpanded}>
-                <div className="grid grid-cols-12 gap-8 pt-2 pb-8 px-4 sm:px-6">
-                    <div className="col-span-12 sm:col-span-3 flex flex-col space-y-3 text-gray-800">
+                <div className="grid grid-cols-12 gap-6 pt-2 pb-8 px-4 sm:px-6">
+                    <div className="col-span-12 sm:col-span-4 lg:col-span-3 xl:col-span-2 pb-6 sm:pb-0 sm:pr-6 flex flex-col space-y-3 border-b sm:border-b-0 sm:border-r border-gray-300 text-gray-800">
                         <div>
                             <p>{dioceseLocation?.dioceseAddress}</p>
                             <p>{`${dioceseLocation?.dioceseCity}, ${dioceseLocation?.dioceseState}`}</p>
@@ -55,7 +55,7 @@ export default function AccordianCard({ diocese }: Props) {
                             </Link>
                         )}
                     </div>
-                    <div className="col-span-12 sm:col-span-9">
+                    <div className="col-span-12 sm:col-span-8 lg:col-span-9 xl:col-span-10">
                         {info ? (
                             <RichText data={info} />
                         ) : (
