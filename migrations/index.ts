f2470d74ece7eaa856bc62031b81fed30c29f232
Christian Callahan <callahan.0019@gmail.com>
2025-04-22 20:06:17 -0400

    edit type migrations

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250422_210311 from './20250422_210311';
+import * as migration_20250422_223825 from './20250422_223825';
 
 export const migrations = [
   {
-    up: migration_20250422_210311.up,
-    down: migration_20250422_210311.down,
-    name: '20250422_210311'
+    up: migration_20250422_223825.up,
+    down: migration_20250422_223825.down,
+    name: '20250422_223825'
   },
 ];
