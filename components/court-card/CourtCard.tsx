cc9c5f569b02add638812c5a11aabbc94a3c47ee
Christian Callahan <callahan.0019@gmail.com>
2025-04-26 17:15:09 -0400

    add conditional rendering

--- components/court-card/CourtCard.tsx
+++ components/court-card/CourtCard.tsx
@@ -9,12 +9,12 @@ export default function CourtCards({ courts }: Props) {
     return (
         <div className="lg:hidden flex flex-col mt-4">
             <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6">
-                <div className="inline-block min-w-full py-2 align-middle md:px-6">
+                <div className="inline-block min-w-full py-2 align-middle px-4 md:px-6">
                     <table className="min-w-full">
                         <tbody className="divide-y divide-gray-200">
                             {courts.map((court, index) => (
                             <tr key={court.id || index}>
-                                <td className="pl-8 py-4 text-sm font-bold text-gray-900 whitespace-nowrap">
+                                <td className="py-4 text-sm font-bold text-gray-900 whitespace-nowrap">
                                     <div className="space-y-1">
                                         <p>{court.courtName}</p>
                                         <p className="text-sm font-medium text-gray-500">#{court.courtNumber}</p>
@@ -23,11 +23,16 @@ export default function CourtCards({ courts }: Props) {
                                     </div>
                                 </td>
 
-                                <td className="pr-8 py-4 text-sm font-medium text-right text-gray-900 whitespace-nowrap">
+                                <td className="py-4 text-sm font-medium text-right text-gray-900 whitespace-nowrap">
                                     <div className="space-y-1">
                                         <p>{`Instituted: ${formatDate(court.instituted)}`}</p>
-                                        <p>{`Court Regent: ${(court.courtOfficers?.courtRegent as Contact).contactName}`}</p>
-                                        <p>{(court.courtOfficers?.courtRegent as Contact).contactEmail}</p>
+                                        {court.courtOfficers && court.courtOfficers.courtRegent && (
+                                            <div className="space-y-1">
+                                                <p>{`Court Regent: ${(court.courtOfficers.courtRegent as Contact).contactName}`}</p>
+                                                <p>{(court.courtOfficers.courtRegent as Contact).contactEmail}</p>
+                                            </div>
+                                        )}
+                                        
                                     </div>
                                 </td>
                             </tr>
