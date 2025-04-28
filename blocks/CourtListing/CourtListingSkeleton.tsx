f8a403098040872cbc84a1e2c6180af96889b73e
Christian Callahan <callahan.0019@gmail.com>
2025-04-28 04:09:24 -0400

    align skeleton more closely with real component

--- blocks/CourtListing/CourtListingSkeleton.tsx
+++ blocks/CourtListing/CourtListingSkeleton.tsx
@@ -6,12 +6,22 @@ import type { CourtListingBlock as CourtListingBlockProps } from "@/payload-type
 export const CourtListingSkeleton: React.FC<CourtListingBlockProps> = (props) => {
     const { richText, rowsPerPage } = props
 
+    const columns = [
+        { header: 'Number', accessor: 'courtNumber' },
+        { header: 'Name', accessor: 'courtName' },
+        { header: 'Diocese', accessor: 'courtDiocese.dioceseName' },
+        { header: 'Location', accessor: 'courtLocation.courtCity' },
+        { header: 'Instituted', accessor: 'instituted' },
+        { header: 'Anniversary', accessor: 'instituted' },
+        { header: 'Court Regent', accessor: 'courtOfficers.courtRegent.contactName' },
+      ];
+
     return (
         <div className="container my-16 sm:my-20">
                 <div className="py-10 px-4 sm:px-6 rounded-xl bg-background/60 backdrop-blur-sm shadow-xl">
                     {richText && <RichText data={richText} />}
-                    <div className="rounded h-5 w-1/3 mb-8 bg-gray-300" />
-                    <TableSkeleton rowsPerPage={rowsPerPage} />
+                    <div className="rounded h-[22px] w-1/3 mb-8 bg-gray-300" />
+                    <TableSkeleton rowsPerPage={rowsPerPage} columns={columns} />
                     <CourtCardsSkeleton rowsPerPage={rowsPerPage} />
                 </div>
             </div>
--- components/table/TableSkeleton.tsx
+++ components/table/TableSkeleton.tsx
@@ -1,8 +1,11 @@
-type Props = {
+import type { Column } from "./Table";
+
+type Props<T> = {
     rowsPerPage: number;
+    columns: Column<T>[]
 }
 
-export default function TableSkeleton({ rowsPerPage }: Props) {
+export default function TableSkeleton<T>({ rowsPerPage, columns }: Props<T>) {
     const rowsArr: number[] = [];
 
     for (let i = 0; i <= rowsPerPage; i++) {
@@ -16,17 +19,30 @@ export default function TableSkeleton({ rowsPerPage }: Props) {
                     <table className="min-w-full divide-y divide-gray-200">
                         <thead className="table-header-group">
                             <tr>
-                                <th>
-                                    <p className="rounded my-4 h-5 w-full bg-gray-400 animate-pulse"></p>
-                                </th> 
+                            {columns.map((column, index) => (
+                                <th key={index} className="py-3.5 pl-4 pr-3 text-left text-sm whitespace-nowrap font-medium text-gray-500 sm:pl-6 md:pl-0">
+                                    <div
+                                        className="flex items-center cursor-pointer"
+                                    >
+                                        {column.header}
+                                        <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M8 9l4-4 4 4m0 6l-4 4-4-4" />
+                                        </svg>
+                                    </div>
+                                </th>
+                            ))}
                             </tr>
                         </thead>
                         <tbody className="divide-y divide-gray-200">
                             {rowsArr.map((row) => (
                                 <tr key={row}>
-                                    <td>
-                                        <p className="rounded my-5 h-5 w-full bg-gray-300 animate-pulse"></p>
-                                    </td>
+                                    {columns.map((column, colIndex) => {
+                                        return (
+                                        <td key={colIndex} className="hidden py-6 pr-3 lg:table-cell">
+                                            <p className="rounded h-[22px] bg-gray-300" ></p>
+                                        </td>
+                                        );
+                                    })}
                                 </tr>
                             ))}
                         </tbody>
